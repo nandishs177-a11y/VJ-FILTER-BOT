4c7379c4d13a9e6f1d477d0bfcda8960f3060e3c
nandishs177-a11y <nandishs177@gmail.com>
2026-03-10 16:00:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ auth_channel = environ.get('AUTH_CHANNEL', '-1003708952638') # give your force s
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 
 # This Channel Is For When User Request Any File Name With command or hashtag like - /request or #request
-reqst_channel = environ.get('REQST_CHANNEL', '')
+reqst_channel = environ.get('REQST_CHANNEL', '-1003708952638')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 
 # This Channel Is For Index Request 
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
