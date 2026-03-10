c2868057382a1b6c0e1330475a951715c386c780
nandishs177-a11y <nandishs177@gmail.com>
2026-03-10 20:06:35 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -256,14 +256,14 @@ Join : @KNMoviesHub17</b>"""
 
 IMDb Data:
 
-<b>🏷 Title</b>: <a href={url}>{title}</a>
+"""<b>🏷 Title</b>: <a href={url}>{title}</a>
 🎭 Genres: {genres}
 📆 Year: <a href={url}/releaseinfo>{year}</a>
 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
 ☀️ Languages : <code>{languages}</code>
 📀 RunTime: {runtime} Minutes
 📆 Release Info : {release_date}
-🎛 Countries : <code>{countries}</code>
+🎛 Countries : <code>{countries}</code>"""
 
 
 ⏰Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
@@ -755,6 +755,7 @@ STREAM_TXT = """<b><u>HOW TO GET STREAM AND DOWNLOAD LINK :</u>
 
 
     
+
 
 
 
