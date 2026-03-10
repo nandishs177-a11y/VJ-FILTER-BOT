6b95edc57542a64ebcb0ef25c362365fdce10605
nandishs177-a11y <nandishs177@gmail.com>
2026-03-10 18:17:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -242,9 +242,9 @@ Nᴀᴍᴇ - {}"""
 
 𝗡𝗮𝗺𝗲 <b>: {}</b>
 
-𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
+𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: 
 
-  CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : @KNMoviesHub17 - {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : @KNMoviesHub17 - {file_name}
 
 ⚙️ sɪᴢᴇ : {file_size}</b>"""
 
@@ -752,6 +752,7 @@ STREAM_TXT = """<b><u>HOW TO GET STREAM AND DOWNLOAD LINK :</u>
 
 
     
+
 
 
 
