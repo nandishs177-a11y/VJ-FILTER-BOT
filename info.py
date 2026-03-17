d2a24bc1ad0fcb27313cbc48cf459b4499d2ae10
nandishs177-a11y <nandishs177@gmail.com>
2026-03-17 15:36:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'https://inshorturl.com/api')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'c1b32d25a37d181f117464d8a9ca962162229732')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
